ba1095ca2ec47a2d3411433ad0d9fbd0da5ebb63
Alexandru Mucenic <41705755+AlexandruMucenic@users.noreply.github.com>
2025-03-31 14:04:06 +0300

    Converted BE to Typescript (#9)

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-Tech Stack:
+# bookshop project backend
+Deployed using Render - https://react-bookshop-backend.onrender.com/
 
-##Front-end
-- HTML
-- CSS
-- React.js
-- React Router 
-
-##Back-end
+Technologies used:
 - Node.js
 - Express.js
 - Mongoose
+
+Created a basic API with two routes: products and cart. Which can receive crud requests from the client.
---
+++ src/routes/cart.ts
@@ -0,0 +1,90 @@
+import { Router, Request, Response } from "express";
+import CartProduct from "../models/cartProduct";
+
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const cartProducts = await CartProduct.find();
+    res.send(cartProducts);
+  } catch (error: any) {
+    res.status(500).send("Error: " + error.message);
+  }
+});
+
+router.delete(
+  "/:id/delete",
+  async (req: Request<{ id: string }>, res: Response) => {
+    try {
+      await CartProduct.deleteOne({ id: req.params.id });
+
+      const cartProducts = await CartProduct.find();
+      res.send(cartProducts);
+    } catch (error: any) {
+      res.status(404).send({ error: "Product doesn't exist!" });
+    }
+  }
+);
+
+router.put("/:id/add", async (req: any, res: any) => {
+  try {
+    const { id, author, title, imageURL, price, quantity } = req.body;
+    const product = await CartProduct.findOne({ id: req.params.id });
+
+    if (!product) {
+      const cartProduct = new CartProduct({
+        _id: id,
+        id,
+        author,
+        title,
+        price,
+        imageURL,
+        quantity,
+      });
+
+      await cartProduct.save();
+      return res.send(cartProduct);
+    }
+
+    await product.updateOne({ $inc: { quantity } });
+
+    const cartProducts = await CartProduct.find();
+    res.send(cartProducts);
+  } catch (error: any) {
+    res.status(404).send({ error: "Product doesn't exist!" });
+  }
+});
+
+router.put(
+  "/:id/increaseQuantity",
+  async (req: Request<{ id: string }>, res: Response) => {
+    try {
+      const product = await CartProduct.findOne({ id: req.params.id });
+
+      await product?.updateOne({ $inc: { quantity: 1 } });
+
+      const updatedProducts = await CartProduct.find();
+      res.send(updatedProducts);
+    } catch (error: any) {
+      res.status(404).send({ error: "Product doesn't exist!" });
+    }
+  }
+);
+
+router.put(
+  "/:id/decreaseQuantity",
+  async (req: Request<{ id: string }>, res: Response) => {
+    try {
+      const product = await CartProduct.findOne({ id: req.params.id });
+
+      await product?.updateOne({ $inc: { quantity: -1 } });
+
+      const updatedProducts = await CartProduct.find();
+      res.send(updatedProducts);
+    } catch (error: any) {
+      res.status(404).send({ error: "Product doesn't exist!" });
+    }
+  }
+);
+
+export default router;
